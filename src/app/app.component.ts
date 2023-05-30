df2742f947f13f43d29621ad9bf56e75c979a8e9
“sandraldr27” <sandralilder@gmail.com>
2023-05-30 20:29:56 +0200

    A new logo is added, work is being done on responsiveness and footer design (Including information and social networks)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,67 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     RouterLink,
     RouterOutlet,
   ],
-  template: `
-    <main style="background-color: #f9f1fe;">
-      <a [routerLink]="['/']">
-        <header class="brand-name" style="margin-top: 50px;" style="background-color; #F9F1FE;">
-          <img class="brand-logo" src="/assets/header/logo.png" alt="logo" aria-hidden="true" width="190" height="190" style="margin-top: -10px;">
-          <i class="fab fa-facebook-f" style="float: right;"></i>
-  <i class="fab fa-twitter" style="float: right;"></i>
-  <i class="fab fa-instagram" style="float: right;"></i>
 
-        </header>
-      </a>
+  template: `
+
+      <style>
+          .brand-logo {
+              width: 100%;
+              height: auto;
+              max-height: 310px;
+          }
+
+          .footer {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+          }
+
+          .social-icons {
+              display: flex;
+              align-items: center;
+          }
+
+          .social-icons a {
+              margin-right: 10px; /* Ajusta el valor según el espacio deseado */
+          }
+
+          .info-footer {
+              text-align: right;
+          }
+      </style>
+
+      <main style="background-color: #f9f1fe">
+          <a [routerLink]="['/']">
+            <header class="brand-name" style="margin-top: 50px" background-color= "#F9F1FE" width= "100%" overflow= "hidden">
+                <img class="brand-logo" src="/assets/header/header.png" alt="logo" aria-hidden="true" style="margin-top: -60px" width= "100%">
+            </header>
+          </a>
+
       <section class="content">
-        <router-outlet></router-outlet>
+          <router-outlet></router-outlet>
       </section>
-    </main>
+
+          <div>
+              <footer class="footer">
+                  <div class="social-icons">
+                      <a href="#" target="_blank"><img src="/assets/footer/facebook.png" alt="Facebook" width="50px" height="50px"></a>
+                      <a href="#" target="_blank"><img src="/assets/footer/twitter.png" alt="Twitter" width="50px" height="50px"></a>
+                      <a href="#" target="_blank"><img src="/assets/footer/instagram.png" alt="Instagram" width="50px" height="50px"></a>
+                      <a href="#" target="_blank"><img src="/assets/footer/linkedin.png" alt="Instagram" width="50px" height="50px"></a>
+                      <a href="#" target="_blank"><img src="/assets/footer/pinterest.png" alt="Instagram" width="50px" height="50px"></a>
+                  </div>
+
+                  <div class="info-footer">
+                      <p><img src="/assets/footer/ubication.png" alt="ubication icon" width="20px" height="20px"> Avenida del Pool Frontend s/n. CP 29651, Mijas - Málaga</p>
+                      <p><img src="/assets/footer/mail.png" alt="mail icon" width="20px" height="20px"> sandraldr@realestatefrontend.com</p>
+                      <p><img src="/assets/footer/phone.png" alt="phone icon" width="20px" height="20px"> +34 123456789</p>
+                      <p>&copy; <b>2023 SANDRALDR Real Estate.</b> Todos los derechos reservados.</p>
+                  </div>
+              </footer>
+          </div>
+
+      </main>
   `,
   styleUrls: ['./app.component.css'],
 })
